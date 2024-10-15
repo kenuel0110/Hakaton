da1bf740923421406dc44ff8795aa237cafb44c9
kenuel0110 <zeroonezero0110@gmail.com>
2024-10-16 00:06:36 +0300

    Add reg to db

--- src/app/models/event.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Event {
-    id: number;
-    owner: string;
-    title: string;
-    description: string;
-    count_people: string;
-    start_arenda: string;
-    end_arenda: string;
-}
