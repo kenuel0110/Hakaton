8d5a573986aa971d0c240824cc419639a7e4f8b2
Sw1ftFox <v.suvorkin2017@gmail.com>
2024-10-15 11:36:58 +0300

    Update city.model.ts

--- src/app/models/city.model.ts
+++ src/app/models/city.model.ts
@@ -1,7 +1,42 @@
-export interface City {
+export interface Request {
     id: number;
-    image: string;
-    name: string;
+    owner: string;
+    title: string;
     description: string;
-    favorite: boolean;
+    special_quest: string;
+    equipment: string;
+    owner_equipment: string;
+    comments: string;
+    start_arenda: string;
+    end_arenda: string;
+    fullname: string;
+    post: string;
+    tel: string;
+    id__room: number;
+    status: number;
+}
+
+export interface Room {
+    id: number;
+    title: string;
+    max_count: number;
+    AR_link: string;
+}
+
+export interface Request {
+    id: number;
+    owner: string;
+    title: string;
+    description: string;
+    count_people: string;
+    start_arenda: string;
+    end_arenda: string;
+}
+
+export interface User {
+    fullname: string;
+    uuid: number;
+    email: string;
+    role: string;
+    owned_id: string;
 }
