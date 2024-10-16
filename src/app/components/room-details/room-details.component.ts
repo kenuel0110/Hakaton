9ed4353f6bf96a8704586587f1600de08fee21e0
kenuel0110 <zeroonezero0110@gmail.com>
2024-10-16 05:45:53 +0300

    iframe

--- src/app/components/room-details/room-details.component.ts
+++ src/app/components/room-details/room-details.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Request } from '../../models/request.model';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
-import {RequestModel} from "../../models/RequestModel.model";
-import {RequestService} from "../../models/request.model";
-import {FirebaseService} from "../../services/firebase.service";
+import { FirebaseService } from "../../services/firebase.service";
+import { DomSanitizer } from '@angular/platform-browser'; // Импортируем DomSanitizer
+
+import { Room } from "../../models/roomnew.model"; // Import the Room interface
 
 @Component({
   selector: 'app-room-details',
@@ -14,18 +13,38 @@ import {FirebaseService} from "../../services/firebase.service";
   styleUrls: ['./room-details.component.css'],
 })
 export class RoomDetailsComponent implements OnInit {
-  request: Request | undefined;
-
-  constructor(private route: ActivatedRoute, private firestore: FirebaseService, private requestService: RequestService, private fb: FormBuilder, private router: Router) {
-    this.route.paramMap.subscribe(params => {
-      const requestId = parseInt(params.get('id') || '', 10);
-      this.request = this.requestService.getRequestById(requestId);
-    });
-  }
-
+  roomId: number | undefined;
+  roomData: Room | undefined;
+  arModelHtml: any; // Для хранения HTML-кода
   form!: FormGroup;
 
+  constructor(
+    private route: ActivatedRoute,
+    private firestore: FirebaseService,
+    private fb: FormBuilder,
+    private router: Router,
+    private sanitizer: DomSanitizer // Инжектируем DomSanitizer
+  ) {}
+
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      this.roomId = parseInt(params.get('id') || '', 10);
+      this.firestore.getData('room').then((data: Room[] | null) => {
+        if (data) {
+          this.roomData = data.find((room: Room) => room.id === this.roomId);
+          if (this.roomData) {
+            // Сохраняем HTML-код
+            this.arModelHtml = this.sanitizer.bypassSecurityTrustHtml(this.roomData.ar_link);
+          }
+        } else {
+          console.log('No data available');
+        }
+      }).catch(error => {
+        console.error('Error fetching data:', error);
+      });
+    });
+
+    // Инициализация формы
     this.form = this.fb.group({
       owner: ['', Validators.required],
       title: ['', Validators.required],
@@ -40,52 +59,46 @@ export class RoomDetailsComponent implements OnInit {
       post: ['', Validators.required],
       tel: ['', Validators.required],
     });
-    this.form.updateValueAndValidity();
   }
 
   onSubmit() {
-    if (this.form.valid) {
+    if (this.form.valid && this.roomData) {
       const formData = this.form.value;
-      console.log(formData); // Temporary check
 
-
-      const requestModel: RequestModel = {
-        userEmail: this.request?.email,
-        adminEmail: 'nesterkov02@gmail.com',
-        request: this.request
+      const requestModel = {
+        id: this.roomData.id,
+        title: formData.title,
+        description: formData.description,
+        fullname: formData.fullname,
+        comments: formData.comments,
+        count_people: 0, // Укажите нужное значение
+        id_room: this.roomData.id,
+        owner: formData.owner,
+        equipment: formData.equipment,
+        owner_equipment: formData.owner_equipment,
+        post: formData.post,
+        special_guest: formData.special_quest,
+        start_date: formData.start_date,
+        end_date: formData.end_date,
+        status: 'pending' // Устанавливаем статус по умолчанию
       };
 
-      // Пример URL, на который отправляется запрос
-      const url = 'https://sobirator.energy-rust.ru/Notification/notification';
-
-      // Отправка POST запроса
-      /*this.http.post(url, requestModel).subscribe({
-        next: (response) => {
-          console.log('Успешный ответ от сервера:', response);
-          // Добавление в Firebase
-          this.firestore.writeData('request', this.request)
-            .then(() => {
-              console.log('Данные успешно добавлены в Firebase');
-              this.form.reset();
-              this.router.navigate(['/main']);
-            })
-            .catch((error) => {
-              console.error('Ошибка при добавлении данных в Firebase:', error);
-            });
+      // Отправка данных в Firebase
+      this.firestore.writeData(`requests/${requestModel.id}`, requestModel)
+        .then(() => {
+          console.log('Данные успешно добавлены в Firebase');
           this.form.reset();
           this.router.navigate(['/main']);
-        },
-        error: (error) => {
-          console.error('Ошибка при отправке данных:', error);
-          // Обработка ошибок
-        }
-      });*/
+        })
+        .catch((error) => {
+          console.error('Ошибка при добавлении данных в Firebase:', error);
+        });
     } else {
-      console.log('Форма не валидна');
+      console.log('Форма не валидна или данные о комнате не найдены');
     }
   }
 
   goBackPage() {
-    this.router.navigate(['/main']);  // Переход на маршрут 'ar'
+    this.router.navigate(['/main']);  // Переход на маршрут 'main'
   }
 }
